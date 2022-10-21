4b4b7b07f5070dccad6a33d2149877d5e994dd80
An Le <anle1@cpp.edu>
2022-10-21 16:42:47 -0700

    Create outline file

---
+++ initial_outline.c
@@ -0,0 +1,26 @@
+// search-multiple-function branch
+
+// function search by phone number
+//     create end pointer
+//     loop until end of table
+//         if string name == target
+//             return index location
+//     return NULL if not found
+
+// function seach by salary
+//     create end pointer
+//     loop until end of table
+//         if double salary == target
+//             return index
+//     return NULL if not found
+
+
+// single-search-pass-function
+
+// create compare functions that return type to compare
+//     1. for phone number
+//     2. for salary
+
+// create function so main function can call
+//     1. for phone number
+//     2. for salary
