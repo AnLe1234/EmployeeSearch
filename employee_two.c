2d1efe2e4c4b24c2c590d14e1fd430e2da4fd62b
An Le <anle1@cpp.edu>
2022-10-21 16:56:09 -0700

    Update employee_two.c to unclude search by phone and salary

--- employee_two.c
+++ employee_two.c
@@ -25,10 +25,26 @@ static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee table
 static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr) {
     return strcmp((char *)targetPtr, tableValuePtr->name);  // const void *targetPtr ==> typecast as char pointer then pass into strcmp()
 }
+
+//quiz 3
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr) {
+    return strcmp((char *)targetPtr, tableValuePtr->phone);  // const void *targetPtr ==> typecast as char pointer then pass into strcmp()
+}
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr) {
+    return *(double *)targetPtr != tableValuePtr->salary;  // const void *targetPtr ==> typecast as int pointer then dereference
+}
 // These are called wrappers. These functions are what you will use in your main!!!
 PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number) {
     return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber);
 }
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char *name) {
     return searchEmployeeTable(ptr, size, name, compareEmployeeName);
+}
+
+// quiz 3
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char *phone) {
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone);
+}
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary) {
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary);
 }
